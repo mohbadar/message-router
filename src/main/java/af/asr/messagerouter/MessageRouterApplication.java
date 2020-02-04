a1c0fc60399e325bf2d0cb83f605e022226a2110
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-04 10:25:27 +0430

    content

--- src/main/java/af/asr/messagerouter/MessageRouterApplication.java
+++ src/main/java/af/asr/messagerouter/MessageRouterApplication.java
@@ -1,15 +1,22 @@
 package af.asr.messagerouter;
 
+import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MessageRouterApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MessageRouterApplication.class, args);
-
 	}
 
+	@Bean
+	public CamelContext camelContext()
+	{
+		return new DefaultCamelContext();
+	}
 }
